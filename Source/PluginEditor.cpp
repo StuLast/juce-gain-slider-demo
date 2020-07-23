0931b8ef26cd3c41cc3f47870159b20f0e98f429
Stu Last <stuart@last-family.co.uk>
2020-07-23 19:38:48 +0100

    Initial commit with minor UI work completed

---
+++ Source/PluginEditor.cpp
@@ -0,0 +1,46 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+Gaintutorial1AudioProcessorEditor::Gaintutorial1AudioProcessorEditor (Gaintutorial1AudioProcessor& p)
+    : AudioProcessorEditor (&p), audioProcessor (p)
+{
+    // Make sure that before the constructor has finished, you've set the
+    // editor's size to whatever you need it to be.
+    setSize (200, 400);
+    
+    gainSlider.setSliderStyle(juce::Slider::SliderStyle::LinearVertical);
+    gainSlider.setTextBoxStyle(juce::Slider::TextBoxBelow, true, 100, 25);
+    gainSlider.setRange(0.0, 1.0);
+    gainSlider.setValue(0.5);
+    addAndMakeVisible(gainSlider);
+    
+}
+
+Gaintutorial1AudioProcessorEditor::~Gaintutorial1AudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void Gaintutorial1AudioProcessorEditor::paint (juce::Graphics& g)
+{
+
+}
+
+void Gaintutorial1AudioProcessorEditor::resized()
+{
+    gainSlider.setBounds(getLocalBounds());
+}
+
+void Gaintutorial1AudioProcessorEditor::sliderValueChanged(juce::Slider* slider)
+{
+   
+}
---
+++ Source/PluginEditor.h
@@ -0,0 +1,37 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+//==============================================================================
+/**
+*/
+class Gaintutorial1AudioProcessorEditor  : public juce::AudioProcessorEditor, juce::Slider::Listener
+{
+public:
+    Gaintutorial1AudioProcessorEditor (Gaintutorial1AudioProcessor&);
+    ~Gaintutorial1AudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    Gaintutorial1AudioProcessor& audioProcessor;
+    juce::Slider gainSlider;
+    juce::Label gainLabel;
+
+    void sliderValueChanged(juce::Slider* slider) override;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Gaintutorial1AudioProcessorEditor)
+};
